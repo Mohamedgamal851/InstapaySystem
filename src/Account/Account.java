a6166abb10d67c7aabb0e66c11eda5cf6d94f25e
AlaaAymanHashem <117596345+AlaaAymanHashem@users.noreply.github.com>
2023-11-27 15:16:52 +0200

    removing folder

--- src/Account/Account.java
+++
@@ -1,9 +0,0 @@
-package Account;
-
-public interface Account {
-    double getBalance();
-    void withdraw(double amount);
-
-
-    void deposit(double amount);
-}
--- src/Services/Government.java
+++
@@ -1,9 +0,0 @@
-package Services;
-
-public class Government implements API {
-    @Override
-    public boolean verify() {
-        // Implementation of verification for Government API
-        return true; // Replace with actual logic
-    }
-}
